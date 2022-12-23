648193659f4d15e07c5a0a06ad096508bfbf8df1
Kieren <kierenhuynh88@gmail.com>
2022-12-23 21:40:40 +1100

    works with multiple digits

--- index.js
+++ index.js
@@ -7,28 +7,32 @@ const clearEl = document.querySelector('#clear')
 
 
 let displayValue;
+let tmpArray = []
 let numbersArray = []
 let symbol = ''
 
 
 numbers.forEach(number => number.addEventListener('click' , (e) => {
-    displayValue = e.target.textContent
+    tmpArray.push(parseInt(e.target.textContent))
+    console.log(tmpArray.join(''))
+    displayValue = tmpArray.join('')
     screenEl.textContent = displayValue
-    numbersArray.push(parseInt(e.target.textContent))
-
 }))
 
 operators.forEach(operator => operator.addEventListener('click', (e) => {
     screen.textContent = e.target.textContent
     symbol = e.target.id
-    console.log(symbol)
+    numbersArray.push(tmpArray.join(''))
+    tmpArray = []
 }))
 
 //event listeners
 equalsEl.addEventListener('click', () => {
+    numbersArray.push(tmpArray.join(''))
     operate(numbersArray[0], numbersArray[1], symbol)
+
+    tmpArray.push(operate(numbersArray[0], numbersArray[1], symbol))
     numbersArray.splice(0, 2)
-    console.log(numbersArray)
 })
 
 clearEl.addEventListener('click', () => {
@@ -36,6 +40,7 @@ clearEl.addEventListener('click', () => {
     screenEl.textContent = ''
     numbersArray = []
     symbol = ''
+    tmpArray = []
 })
 
 
@@ -45,17 +50,19 @@ clearEl.addEventListener('click', () => {
 
 
 // operations
-const add = (a, b) => a + b
+const add = (a, b) => parseInt(a) + parseInt(b)
 const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
+const modulo = (a, b) => a % b
 
 const operations = (a, b) => {
     return {
         'add': add(a, b),
         'subtract': subtract(a, b),
         'multiply': multiply(a, b),
-        'divide': divide(a, b)
+        'divide': divide(a, b),
+        'modulo': modulo(a, b),
     }
 }
 const operate = (a, b, operator) => {
